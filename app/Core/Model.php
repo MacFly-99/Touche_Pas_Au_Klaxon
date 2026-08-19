fdce902da77354565c2b0b6c0cb669819031f301
MacFly <matteo.ventura1999@gmail.com>
2026-08-19 23:56:51 +0700

    Create the files for the MVC Core of the application

---
+++ app/Core/Model.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Core;
+
+use App\Config\Database;
+
+class Model
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+}
+
+?>
